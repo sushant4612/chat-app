5a9a933d0c9dfb2811ef5aceaed298dde13ba0ae
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-07 13:20:47 +0530

    Backend Completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,6 +27,49 @@ wss.on('connection', (socket: UserSocket) => {
     //joining room
     socket.on('join', (roomName: string) => {
         console.log(`User joining room: ${roomName}`);
-        
+        if(socket.currentRoom && rooms[socket.currentRoom]){
+            rooms[socket.currentRoom] = rooms[socket.currentRoom].filter(client => client !== socket);
+            console.log(`Removed user from ${socket.currentRoom}`);
+        }
+
+        if(!rooms[roomName]){
+            rooms[roomName] = [];
+        }
+
+        rooms[roomName].push(socket);
+        socket.currentRoom = roomName;
+
+        console.log(`${roomName} now has ${rooms[roomName].length} members`);
+        socket.send(`Joined ${roomName}`);
+
+        broadcastMessage(socket, `${roomName} now has ${rooms[roomName].length} members`)
+    });
+
+    socket.on('leave', (roomName: string) => {
+        if(socket.currentRoom && rooms[socket.currentRoom]){
+            console.log(`Leaving ${roomName}`);
+            rooms[socket.currentRoom] = rooms[socket.currentRoom].filter(client => client !== socket);
+        }
+        socket.send(`Left the room ${roomName}`)
+    })
+
+    socket.on('close', () => {
+        console.log('User Disconnected');
+        if(socket.currentRoom && rooms[socket.currentRoom]){
+            rooms[socket.currentRoom] = rooms[socket.currentRoom].filter(client => client !== socket)
+        }
     })
 })
+
+const broadcastMessage = (senderSocket: UserSocket, msg: string) => {
+    if (senderSocket.currentRoom && rooms[senderSocket.currentRoom]) {
+        rooms[senderSocket.currentRoom].forEach((client) => {
+            if (client.readyState === 1 && client !== senderSocket) {
+                client.send(msg);
+            }
+        });
+    }
+};
+
+
+console.log('Server is running on ws://localhost:8000');
